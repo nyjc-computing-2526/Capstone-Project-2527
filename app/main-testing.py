adbdab67d4df3918ac6e792c419af39e19f6f618
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2026-04-06 18:21:10 +0800

    finished landing, login and register html pages

---
+++ app/main-testing.py
@@ -0,0 +1,17 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing():
+    return render_template("landing.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+app.run()
