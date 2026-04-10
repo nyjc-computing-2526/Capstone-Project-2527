7e1f2a1c34883d4f2dd034212ca1fb719de75a3d
SavioNYJC <savio.sidharta_2025@nyjc.edu.sg>
2026-04-10 04:20:55 +0000

    feat: implementation of login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_login import LoginManager
+
+from app.models.user import User
+from app.resources.users import get_user_by_id
 from .routes.landing import bp as landing_bp
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(landing_bp) 
+    login_manager.init_app(app)
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    row = get_user_by_id(user_id)
+    if row is None:
+        return None
+    return User(row['id'], row['name'], row['email'], row['password'])
