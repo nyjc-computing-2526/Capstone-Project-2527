859cb2bdf20a86287458c110efcba1bf8b2d8bf5
Micole-NYJC <micole.koh.eu.xuen_2025@nyjc.edu.sg>
2026-04-10 07:14:26 +0000

    landing.py (micole)

--- app/routes/landing.py
+++ app/routes/landing.py
@@ -1,7 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('landing', __name__)
 @bp.route('/')
 def index():
   return render_template('landing.html')
+
+@bp.route('/about', methods=["GET"])
+def about():
+  if request.method == "GET":
+    return render_template('about.html')
+
+@bp.route('/contact', methods=["GET","POST"])
+def contact():
+  if request.method == "GET":
+    return render_template('contact.html')
+  else:
+    if request.method == "POST":
+      pass
+
+
+@bp.route('/features', methods=["GET"])
+def features():
+  if request.method == "GET":
+    return render_template('features.html')
+
+
+
 
