85bbac72b6755da6d26c3f3dc0b4cf8ee3ea7aa2
tiffany <christella.tiffany.tang_2025@nyjc.edu.sg>
2026-04-11 15:57:12 +0000

    register user and verify their email and view profile route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,11 +2,9 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
-
 import secrets
 import resend
 import os
-
 from app.resources.users import UsersResource
 from app.models.user import User
 
@@ -25,14 +23,21 @@ def login():
         
         try:
             user_data = users_resource.authenticate(email, password)
+
+            if not user_data.get("is_verified"):
+                flash("Please verify your email before logging in.", "error")
+                return render_template('login.html')
+            
             user = User(user_data['id'], user_data['name'], user_data['email'], user_data['user_class'], user_data['password'])
+            #is passing password like that safe...?
             login_user(user) 
-            return redirect(url_for('activities.activities'))
+            return redirect(url_for('landing.homepage'))
         except ValueError as e:
             flash(str(e), "error")
             return render_template('login.html')
 
     return render_template('login.html')
+
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -47,25 +52,80 @@ def register():
         if password != confirm_password:
             flash("Passwords do not match", "error")
             return render_template('register.html')
-
+        
         user_data = {'email': email, 'password': password, 'name': name, 'user_class': user_class}
         
         try:
-            users_resource.register(user_data)
-            flash("Account registered successfully", "success")
-            return redirect(url_for('auth.login'))
-        except ValueError as e:
-            flash(str(e), "error")
+            token = secrets.token_urlsafe(32)
+            expires_at = datetime.now(timezone.utc) + timedelta(minutes=15)
+            
+            user = users_resource.register(user_data)
+            id = user['id']
+            user_resource = users_resource.user(id)
+            user_resource.create_verification_token(token, expires_at, 'verify_email')
+            
+            verify_url = f"{request.host_url}auth/verify-email?token={token}"
+            resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": email,
+                "subject": "Verify Email",
+                "html": f"<p>Click to verify your email: <a href='{verify_url}'>Verify Email</a></p>"
+            })
+            flash("Please check your email to verify your account for creation :).", "info")
             return render_template('register.html')
 
-    return render_template('register.html')
+        except Exception as e:
+            flash(str(e), "error")
+            print(str(e))
+            return render_template('register.html')
+
+
+@bp.route('/verify-email')
+def verify_email():
+    """verifies user email"""        
+    token = request.args.get("token")
+
+    if not token:
+        return "Missing token", 400
+
+    verify = users_resource.verify_token(token, 'verify_email')
+
+    if not verify:
+        return "Invalid token", 400
+
+    if verify["expiry"] < datetime.now(timezone.utc):
+        return "Token expired", 400
     
+    id = verify["user_id"]
+    user_resource = users_resource.user(id)
+
+    try:
+        user_resource.update({"is_verified": True})
+        users_resource.invalidate_token(token)
+
+        flash("Email verified successfully! Your account has been created!", "success")
+        return redirect(url_for('auth.login'))
+
+    except Exception as e:
+        flash(str(e), "error")
+        return redirect(url_for('auth.register'))
+
+
 @bp.route('/logout', methods=['POST'])
 @login_required
 def logout():
     logout_user()
     flash("Logged out successfully", "success")
     return redirect(url_for('auth.login'))
+
+
+@bp.route('/view/<int:id>')  
+@login_required
+def view_profile(id):
+    """allows user to view their profile details"""
+    user_resource = users_resource.user(id)
+    user_data = user_resource.get()
+    return render_template('view_profile.html', user_data=user_data)
 
 
 @bp.route('/update/<int:id>', methods=['GET', 'POST'])  
@@ -124,7 +184,7 @@ def delete_user(id):
         return redirect(url_for('landing.landing'))
     except ValueError as e:
         flash(str(e), "error")
-        return redirect(url_for('activities.activities'))
+        return redirect(url_for('landing.homepage'))
     
 @bp.route('/forgot-password', methods=['GET', 'POST'])  
 def forgot_password():
