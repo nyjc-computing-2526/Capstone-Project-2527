1063078155b366286609a60f0d109d8f8450d4ef
SavioNYJC <gross.savio@gmail.com>
2026-04-20 15:25:35 +0800

    feat: fix docker deployment and dev separation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+
 import app
 
 capstone = app.create_app()
 
 if __name__ == "__main__":
-    capstone.run(host="0.0.0.0")
+    capstone.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
